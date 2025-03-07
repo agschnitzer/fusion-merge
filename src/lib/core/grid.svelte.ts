fdce1a8eec5ed852afd6250477427410a16f60cf
Alex Gschnitzer <75177215+agschnitzer@users.noreply.github.com>
2025-03-07 23:01:11 +0100

    Restructure game logic into store-like structure (#1)
    Create random starting position. Add necessary types.

---
+++ src/lib/core/grid.svelte.ts
@@ -0,0 +1,154 @@
+import type { Grid, GridState, Tile } from '$lib/types/grid.type'
+
+export const createGrid = (size: number): GridState => {
+  const state: Grid = Array.from({ length: size }, () => Array.from({ length: size }, () => null))
+  const positions = {
+    up: {
+      start: 0,
+      end: state.length,
+      step: 1,
+    },
+    down: {
+      start: state.length - 1,
+      end: 0,
+      step: -1,
+    },
+    left: {
+      start: 0,
+      end: state.length,
+      step: 1,
+    },
+    right: {
+      start: state.length - 1,
+      end: 0,
+      step: -1,
+    },
+  }
+
+  let emptyTiles: Record<string, Omit<Tile, 'value'>> = {}
+  let moved = false
+
+  // Initialize grid with two random tiles
+  for (let i = 0; i < 2; i++) {
+    const x = Math.floor(Math.random() * size)
+    const y = Math.floor(Math.random() * size)
+
+    if (state[y][x]) {
+      i--
+      continue
+    }
+
+    state[y][x] = { value: Math.random() < 0.9 ? 2 : 4, x, y }
+  }
+
+  return {
+    /**
+     * Returns the grid.
+     * @since 1.0.0
+     * @version 1.0.0
+     *
+     * @returns {Grid} The grid state.
+     */
+    get grid(): Grid { return state },
+    /**
+     * Returns the empty tiles.
+     * @since 1.0.0
+     * @version 1.0.0
+     *
+     * @returns {Record<string, Omit<Tile, 'value'>>} The empty tiles.
+     */
+    get emptyTiles(): Record<string, Omit<Tile, 'value'>> { return emptyTiles },
+    /**
+     * Returns whether a tile has moved since the last move.
+     * @since 1.0.0
+     * @version 1.0.0
+     *
+     * @returns {boolean} `true` if a tile has moved, `false` otherwise.
+     */
+    get moved(): boolean { return moved },
+    /**
+     * Updates the grid by moving tiles.
+     * @since 1.0.0
+     * @version 1.0.0
+     *
+     * @param {'up' | 'down' | 'left' | 'right'} direction The direction to move the tiles.
+     */
+    update: (direction: 'up' | 'down' | 'left' | 'right'): void => {
+      moved = false
+
+      const { start, end, step } = positions[direction]
+      const horizontal = direction === 'left' || direction === 'right'
+
+      emptyTiles = {}
+      for (let i = 0; i < state.length; i++) {
+        let position = start
+        let lastMerged: Tile | null = null
+
+        for (let j = start; direction === 'down' || direction === 'right' ? j >= end : j < end; j += step) {
+          const y = horizontal ? i : j
+          const x = horizontal ? j : i
+
+          const tile = state[y][x]
+          if (!tile) {
+            emptyTiles[`${ y }${ x }`] = { x, y }
+            continue
+          }
+
+          tile.y = y
+          tile.x = x
+
+          const newY = horizontal ? i : position
+          const newX = horizontal ? position : i
+
+          // Move tile to new position
+          if (position !== j) {
+            state[y][x] = null
+            state[newY][newX] = tile
+
+            emptyTiles[`${ y }${ x }`] = { x, y }
+            delete emptyTiles[`${ newY }${ newX }`]
+
+            moved = true
+          }
+
+          const targetY = horizontal ? i : position + step * (-1)
+          const targetX = horizontal ? position + step * (-1) : i
+
+          if (targetY < 0 || targetX < 0 || targetY >= state.length || targetX >= state.length) {
+            position += step
+            continue
+          }
+
+          const targetTile = state[targetY][targetX]
+
+          // Merge only if target tile has not been merged before
+          if (targetTile && lastMerged !== targetTile && tile.value === targetTile.value) {
+            state[newY][newX] = null
+            state[targetY][targetX]!.value *= 2
+
+            emptyTiles[`${ newY }${ newX }`] = { x: newX, y: newY }
+            delete emptyTiles[`${ targetY }${ targetX }`]
+
+            lastMerged = state[targetY][targetX]
+            moved = true
+
+            continue
+          }
+
+          position += step
+        }
+      }
+    },
+    /**
+     * Adds a new tile to the grid.
+     * @since 1.0.0
+     * @version 1.0.0
+     */
+    addTile: (): void => {
+      const emptyTilesArray = Object.values(emptyTiles)
+      const { y, x } = emptyTilesArray[Math.floor(Math.random() * emptyTilesArray.length)]!
+      state[y][x] = { value: Math.random() < 0.9 ? 2 : 4, y, x }
+      delete emptyTiles[`${ y }${ x }`]
+    },
+  }
+}
--- src/lib/types/grid.type.ts
+++ src/lib/types/grid.type.ts
@@ -1,4 +1,24 @@
 /**
+ * Represents the state of the grid.
+ * @since 1.0.0
+ * @version 1.0.0
+ *
+ * @interface GridState
+ * @property {Grid} grid The grid.
+ * @property {Record<string, Omit<Tile, 'value'>>} emptyTiles The empty tiles.
+ * @property {boolean} moved Whether any tiles have moves since the last update.
+ * @property {(direction: 'up' | 'down' | 'left' | 'right') => void} update Updates the grid.
+ * @property {() => void} addTile Adds a new tile to the grid.
+ */
+export interface GridState {
+  grid: Grid
+  emptyTiles: Record<string, Omit<Tile, 'value'>>
+  moved: boolean
+  update: (direction: 'up' | 'down' | 'left' | 'right') => void
+  addTile: () => void
+}
+
+/**
  * Represents a grid of tiles. Each tile is either a {@link Tile} or null when empty.
  * @since 1.0.0
  * @version 1.0.0
