c927af9ddd035bc53cb339d10ce2443beb4a5ab3
Alex Gschnitzer <75177215+agschnitzer@users.noreply.github.com>
2025-03-08 13:34:06 +0100

    Add missing docs, clean up code (#1)

--- src/lib/types/canvas.type.ts
+++ src/lib/types/canvas.type.ts
@@ -4,16 +4,14 @@
  * @version 1.0.0
  *
  * @interface CanvasState
- * @property {boolean} animating Whether the canvas is drawing or not.
+ * @property {boolean} animating Indicates if the canvas is animating.
  * @property {number} animationDuration The duration of the animation.
- * @property {HTMLCanvasElement} element The canvas element.
  * @property {(t?: number) => void} draw Draws the tiles on the canvas.
  * @property {(time: number, startTime: number) => void} animate Animates the tiles on the canvas.
  */
 export interface CanvasState {
   animating: boolean
   animationDuration: number
-  element: HTMLCanvasElement
   draw: (t?: number) => void
   animate: (time: number, startTime: number) => void
 }
