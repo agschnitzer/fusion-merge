d028bcf28eefeb34a9262d9f6960b3968415cd77
Alex Gschnitzer <75177215+agschnitzer@users.noreply.github.com>
2025-03-08 14:30:11 +0100

    Add separate files for different components (#1)

---
+++ src/lib/components/Controls.svelte
@@ -0,0 +1,6 @@
+<!-- @component Component that renders the controls for the game -->
+<script lang="ts">
+  let { reset } = $props()
+</script>
+
+<button onclick={reset} class="px-3 py-2 font-medium bg-slate-700 text-slate-50 rounded-md">New Game</button>
---
+++ src/lib/components/Score.svelte
@@ -0,0 +1,6 @@
+<!-- @component Component that renders the score of the game -->
+<script lang="ts">
+  let { score } = $props()
+</script>
+
+<p class="px-3 flex items-center font-semibold text-2xl text-center text-slate-700 bg-slate-100 rounded-md">Score: {score}</p>
