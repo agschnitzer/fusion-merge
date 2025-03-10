09515482a7b8912a84c64bf165f4a36bde5f2e56
Alex Gschnitzer <75177215+agschnitzer@users.noreply.github.com>
2025-03-10 16:03:43 +0100

    Add breakpoint to line height of best score (#3)

--- src/lib/components/Score.svelte
+++ src/lib/components/Score.svelte
@@ -5,6 +5,6 @@
 
 <div class="xs:min-w-32 p-4 flex xs:block justify-between text-2xl uppercase bg-highlight rounded-lg">
   <p class="xs:leading-5">Score {score}</p>
-  <p class="xs:text-xl leading-4">Best {bestScore}</p>
+  <p class="xs:text-xl xs:leading-4">Best {bestScore}</p>
 </div>
 
