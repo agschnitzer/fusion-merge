ab793b5d06caf6bad8b07f9a0415b9a97e888103
Alex Gschnitzer <75177215+agschnitzer@users.noreply.github.com>
2025-03-13 12:01:28 +0100

    Add link in footer, add label class (#5)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -84,7 +84,9 @@
   <footer class="py-4 flex flex-wrap justify-between items-center 2xs:gap-x-4">
     {#if page.url.pathname === '/'}
       <a href="/about/" class="heading-3">About the game</a>
+    {:else}
+      <a href="/" class="heading-3">Back to game</a>
     {/if}
-    <p class="text-xs text-text font-sans">&copy; {new Date().getFullYear()} Alex Gschnitzer</p>
+    <p class="label">&copy; {new Date().getFullYear()} Alex Gschnitzer</p>
   </footer>
 </div>
