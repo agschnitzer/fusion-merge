95ca7be0945eea9acdc0da276ef2cb7d21ce1ca6
Alex Gschnitzer <75177215+agschnitzer@users.noreply.github.com>
2025-03-14 11:32:58 +0100

    Remove export from `dateToString` (#7)

--- src/lib/components/elements/DateLabel.svelte
+++ src/lib/components/elements/DateLabel.svelte
@@ -10,8 +10,10 @@
    * @param {string} date The date string to convert.
    * @returns {string} A human-readable date string.
    */
-  export const dateToString = (date: string): string => new Date(date).toLocaleDateString('en-US', {
-    year: 'numeric', month: 'long', day: 'numeric',
+  const dateToString = (date: string): string => new Date(date).toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
   })
 </script>
 
